236fcccaaf7560e567b759c645bb06d6f5022a66
zaib <zaibilahishamsi@gmail.com>
2025-09-20 15:31:12 +0530

    cors fixed

--- server/server.js
+++ server/server.js
@@ -8,9 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-}));
+app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
